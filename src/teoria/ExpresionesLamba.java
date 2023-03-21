1ebe79830f9a380f8dc08526926a920ed9919d41
michelxd456 <michelbalvinor@gmail.com>
2023-03-21 18:25:02 -0500

    nuevo commit

---
+++ src/teoria/ExpresionesLamba.java
@@ -0,0 +1,49 @@
+package teoria;
+
+import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ExpresionesLamba {
+
+	public static void main(String[] args) {
+		// Streams / Lamdba
+		
+		List<String> cities = new ArrayList<>();
+		cities.add("London");
+		cities.add("New York");
+		cities.add("Lima");
+		cities.add("Miami");
+		cities.add("Bogota");
+		/* con for normal
+		for (int i = 0; i < cities.size(); i++) {
+			System.out.println(cities.get(i));
+		}*/
+		/* foreach
+		for(String city : cities) {
+			System.out.println(city);
+		}*/
+		/*En Java, un Stream es una secuencia de elementos que se puede procesar 
+		 * de forma paralela o en serie. Un Stream se puede crear a 
+		 * partir de una colección, un array, un archivo, una fuente de datos externa, entre otros.*/
+		List<Integer> numeros = Arrays.asList(2, 4, 6, 8, 10, 7, 3, 1);
+
+		int suma = numeros.stream()          // Creamos un Stream a partir de la lista de números
+		                 .filter(n -> n > 5) // Filtramos los números mayores que 5
+		                 .filter(n -> n % 2 == 0) // Filtramos los números pares
+		                 .mapToInt(n -> n)  // Convertimos los números en enteros
+		                 .sum();            // Sumamos los números
+
+		System.out.println("La suma de los números pares y mayores que 5 es: " + suma);
+
+		cities.stream().parallel().forEach(city -> System.out.println(city));
+		
+		
+	}
+	//Seria igual
+	public static void printCity(String city) {
+		System.out.println(city);		
+	}
+			
+}
